518d990ee759fad52866d1309036485877c9b895
Nisan İpek Akkuzu <74313161+akkuzu@users.noreply.github.com>
2021-03-06 23:42:29 +0300

    Add files via upload

---
+++ BlackCubeSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackCubeSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private GameObject blackcube;
+ 
+
+    void Start()
+    {
+        for (int i = 0; i < 250; i++)
+        {
+            GameObject cube = Instantiate(blackcube, transform.position, Quaternion.identity);
+            //cube.transform.position = new Vector3(1f, 1f, 1f);
+            cube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            
+        }
+
+
+    }
+
+    
+}
+
+
+
---
+++ CubeSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [Space]
+    [SerializeField]
+    private Sprite sprite;
+
+    [Space]
+    [SerializeField]
+    private GameObject cube;
+
+    [Space]
+    [SerializeField]
+    private float offset = 0.1f;
+
+    [SerializeField]
+    float size = .1f;
+
+    Vector3 cubePos = Vector3.zero;
+
+    private void Awake()
+    {
+        CreateCubesFromImage(sprite.texture);
+    }
+
+    void CreateCubesFromImage(Texture2D image)
+    {
+        for (int x = 0; x < image.width; x++)
+        {
+            for (int y = 0; y < image.height; y++)
+            {
+                Color color = image.GetPixel(x, y);
+
+                if (color.a == 0)
+                {
+                    continue;
+                }
+
+                cubePos = new Vector3(size * (x - (image.width * .5f)), size * .5f, size * (y - (image.height * .5f)));
+                GameObject cubeObj = Instantiate(cube, transform);
+                cubeObj.transform.localPosition = cubePos;
+                cubeObj.GetComponent<Renderer>().material.color = color;
+                cubeObj.transform.localScale = Vector3.one * size;
+                    
+            }
+        }
+    }
+}       
+
+   
